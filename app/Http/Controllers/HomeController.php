d5aebd4fcdcf791f98f8a549aa810799ebdb09fe
Jose Luis <joseluisrojasaranda@gmail.com>
2021-05-08 09:29:05 -0500

    User seeder and new app flow

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -13,7 +13,7 @@ class HomeController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth');
+        //$this->middleware('auth');
     }
 
     /**
@@ -22,6 +22,22 @@ class HomeController extends Controller
      * @return \Illuminate\Contracts\Support\Renderable
      */
     public function index()
+    {
+        if (auth()->user()) 
+        {
+            return redirect(route('home'));
+        } else 
+        {
+            return view('welcome');
+        }
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function home()
     {
         return view('home');
     }
